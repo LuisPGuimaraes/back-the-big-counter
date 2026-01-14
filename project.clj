1dcb357124a39c229a32b3cb36f0f8eb45205b53
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-13 21:57:18 -0300

    Add Pedestal dependencies to project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [io.pedestal/pedestal.service "0.6.4"]
+                 [io.pedestal/pedestal.jetty "0.6.4"]]
   :main ^:skip-aot back-end.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
