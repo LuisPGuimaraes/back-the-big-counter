8a882641c01de2f08c6536589d28536fc1fef211
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-13 22:05:09 -0300

    Refactor server structure: move health check route and service definition to http.clj

--- src/back_end/core.clj
+++ src/back_end/core.clj
@@ -1,39 +1,11 @@
 (ns back-end.core
   (:gen-class)
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
-
-(defn health-handler
-  [_request]
-  {:status 200
-   :headers {"content-type" "text/plain"}
-   :body "ok"})
-
-(def routes
-  (route/expand-routes
-   #{["/health" :get health-handler :route-name :health]}))
-
-(def service
-  {::http/routes routes
-   ::http/type :jetty
-   ::http/port 3000
-   ::http/join? true})
-
-(defonce server (atom nil))
-
-(defn start
-  []
-  (let [srv (-> service http/create-server http/start)]
-    (reset! server srv)
-    srv))
-
-(defn stop
-  []
-  (when-let [srv @server]
-    (http/stop srv)
-    (reset! server nil)))
+            [back-end.http :as app-http]))
 
 (defn -main
   [& _args]
   (println "Starting Pedestal server on port 3000.")
-  (start))
+  (-> app-http/service
+     http/create-server
+     http/start))
