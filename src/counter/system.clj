0d5bb2259e6e3bdd7427e304cfa351718060abb5
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-14 00:29:03 -0300

    feat: add schema namespace and refactor system initialization

--- src/counter/system.clj
+++ src/counter/system.clj
@@ -1,5 +1,6 @@
 (ns counter.system
-  (:require [counter.http.routes :as routes]
+  (:require [counter.db.datomic :as db]
+            [counter.http.routes :as routes]
             [io.pedestal.http :as http]))
 
 (def service
@@ -8,6 +9,11 @@
    ::http/port 3000
    ::http/join? true})
 
+(defn create-system []
+  {:db (db/connect)
+   :server (http/create-server service)})
+
 (defn start []
-  (println "Starting server on port 3000")
-  (http/start (http/create-server service)))
+  (let [system (create-system)]
+    (println "Starting server on port 3000")
+    (update system :server http/start)))
