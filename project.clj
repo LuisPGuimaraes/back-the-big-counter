b63087ea4a275446ac132325c8c621fd9e005432
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-13 23:55:19 -0300

    fix: update main namespace in project.clj and add core functionality

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [io.pedestal/pedestal.service "0.6.4"]
                  [io.pedestal/pedestal.jetty "0.6.4"]]
-  :main back-end.core)
+  :main counter.core)
