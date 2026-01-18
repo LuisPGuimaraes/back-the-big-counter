9287d3a174eebe30eb166f796cdbe20f9986af29
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-18 14:17:46 -0300

    refactor: remove user schema from counter schema definition

--- src/counter/infra/db/datomic/schema.clj
+++ src/counter/infra/db/datomic/schema.clj
@@ -6,6 +6,11 @@
     :db/valueType   :db.type/long
     :db/cardinality :db.cardinality/one
     :db/doc         "A counter value"}
+   
+   {:db/ident       :counter/name
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Name of the counter"} 
 
    {:db/ident       :counter/updated-at
     :db/valueType   :db.type/instant
@@ -15,36 +20,9 @@
    {:db/ident       :counter/action
     :db/valueType   :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc         "The action performed on the counter"}
+    :db/doc         "The action performed on the counter"}])
 
-   {:db/ident       :counter/user-id
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc         "Identifier for the user associated with the counter"}])
-
-(def user-schema
-  [{:db/ident       :user/username
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/unique      :db.unique/identity
-    :db/doc         "The username of the user"}
-
-   {:db/ident       :user/email
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/unique      :db.unique/identity
-    :db/doc         "The email of the user"} 
-   
-   {:db/ident       :user/password-hash
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc         "Hashed password of the user"}
-   
-   {:db/ident       :user/token
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/doc         "Authentication token for the user"}])
 
 (defn apply-schema!
   [conn]
-  (d/transact conn {:tx-data (concat counter-schema user-schema)}))
+  (d/transact conn {:tx-data (concat counter-schema)}))
