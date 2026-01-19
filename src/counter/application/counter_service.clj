0d8079c1948db165f35c516b9cf698b5f87a6866
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-19 00:50:16 -0300

    refactor: improve counter service and repository functions for better readability and error handling

--- src/counter/application/counter_service.clj
+++ src/counter/application/counter_service.clj
@@ -1,16 +1,18 @@
 (ns counter.application.counter-service
-  (:require [clojure.string :as string]
-            [counter.infra.db.counter-repo :as repo]))
+  (:require [counter.infra.db.counter-repo :as repo]
+            [datomic.client.api :as d]))
 
 (defn get-count
   [conn counter-id]
   (println "[service] get-count called")
-  (repo/get-count-by-id conn counter-id))
+  (let [db (d/db conn)] 
+    (:counter/value (repo/find-counter-by-id db counter-id))))
 
 (defn increment!
   [conn counter-id increment-value]
-  (when (nil? (repo/find-counter-by-id conn counter-id))
-    (throw (ex-info "counter not found" {:type :counter-not-found})))
+  (let [db (d/db conn)]
+    (when (nil? (repo/find-counter-by-id db counter-id))
+      (throw (ex-info "counter not found" {:type :counter-not-found}))))
   (when (<= increment-value 0)
     (throw (ex-info "increment value must be greater than 0" {:type :invalid-increment})))
   (println "[service] increment! called with value:" increment-value)
@@ -19,29 +21,34 @@
 (defn reset!
   [conn counter-id]
   (println "[service] reset! called")
-  (when (nil? (repo/find-counter-by-id conn counter-id))
-    (throw (ex-info "counter not found" {:type :counter-not-found})))
-  (repo/save-count! conn 0 counter-id))
+  (let [db (d/db conn)]
+    (when (nil? (repo/find-counter-by-id db counter-id))
+      (throw (ex-info "counter not found" {:type :counter-not-found}))))
+  (repo/save-count! conn counter-id 0 "reset"))
 
 (defn get-counters
   [conn]
   (println "[service] get-counters called")
-  (map (fn [[name id]]
-         {:name name
-          :id id})
-       (repo/list-counters conn)))
+  (let [db (d/db conn)]
+    (map (fn [counter]
+           {:id (:db/id counter)
+            :name (:counter/name counter)
+            :value (:counter/value counter)})
+         (repo/list-enabled-counters db))))
 
 (defn create-counter
   [conn name]
   (println "[service] create-counter called")
-  (when (repo/find-counter-by-name conn name)
-    (throw (ex-info "counter already exists" {:type :counter-already-exists})))
+  (let [db (d/db conn)]
+    (when (repo/find-enabled-counter-by-name db name)
+      (throw (ex-info "counter already exists" {:type :counter-already-exists}))))
   (repo/create-counter! conn name))
 
 (defn delete-counter
   [conn id]
   (println "[service] delete-counter called")
-  (let [counter (repo/find-counter-by-id conn id)]
+  (let [db (d/db conn)
+        counter (repo/find-counter-by-id db id)]
     (when (nil? counter)
       (throw (ex-info "counter not found" {:type :counter-not-found})))
     (repo/disable-counter! conn id)))
--- src/counter/infra/db/counter_repo.clj
+++ src/counter/infra/db/counter_repo.clj
@@ -1,89 +1,73 @@
 (ns counter.infra.db.counter-repo
   (:require [datomic.client.api :as d]))
 
-(defn save-count!
-  [conn value counter-id]
-  (println "[repo] save-count! ->" value)
-  (d/transact conn
-              {:tx-data [{:counter/value value
-                          :db/id counter-id
-                          :counter/updated-at (java.util.Date.)}]})
-  value)
+(def counter-pull
+  [:db/id
+   :counter/name
+   :counter/value
+   :counter/enabled
+   :counter/updated-at])
 
-(defn get-count-by-id
-  [conn counter-id]
-  (let [db (d/db conn)]
-    (ffirst
-     (d/q '[:find ?value
-            :in $ ?id
-            :where
-            [?id :counter/value ?value]
-            [?id :counter/enabled true]]
-          db
-          counter-id))))
-
-(defn increment-by!
-  [conn counter-id increment-value]
-  (let [current (get-count-by-id conn counter-id)
-        next (+ current increment-value)]
-    (save-count! conn next counter-id)))
+(defn now []
+  (java.util.Date.))
 
 (defn create-counter!
   [conn name]
-  (let [value 0]
-    (println "[repo] create-counter! ->" name)
-    (let [tx (d/transact conn
-                         {:tx-data [{:counter/name name
-                                     :counter/value value
-                                     :counter/updated-at (java.util.Date.)
-                                     :counter/action "create"
-                                     :counter/enabled true}]})
-          id (ffirst
-              (d/q '[:find ?e
-                     :in $ ?name
-                     :where
-                     [?e :counter/name ?name]
-                     [?e :counter/enabled true]]
-                   (:db-after tx)
-                   name))]
-      {:name name
-       :id id})))
-
-(defn list-counters
-  [conn]
-  (let [db (d/db conn)
-        results (d/q '[:find ?name ?e
-                       :where
-                       [?e :counter/name ?name]
-                       [?e :counter/enabled true]]
-                     db)]
-    results))
-
-(defn find-counter-by-name
-  [conn name]
-  (let [db (d/db conn)]
-    (ffirst
-     (d/q '[:find ?e
-            :in $ ?name
-            :where
-            [?e :counter/name ?name]
-            [?e :counter/enabled true]]
-          db
-          name))))
+  (let [tx @(d/transact conn
+                        {:tx-data [{:counter/name name
+                                    :counter/value 0
+                                    :counter/enabled true
+                                    :counter/action "create"
+                                    :counter/updated-at (now)}]})
+        id (-> tx :tempids vals first)]
+    {:id id
+     :name name}))
 
 (defn find-counter-by-id
-  [conn id]
-  (let [db (d/db conn)]
-    (when (number? id)
-      (let [entity (d/pull db [:db/id :counter/name :counter/enabled] id)]
-        (when (= true (:counter/enabled entity))
-          entity)))))
+  [db id]
+  (when (number? id)
+    (d/pull db counter-pull id)))
+
+(defn find-enabled-counter-by-name
+  [db name]
+  (ffirst
+   (d/q '[:find ?e
+          :in $ ?name
+          :where
+          [?e :counter/name ?name]
+          [?e :counter/enabled true]]
+        db
+        name)))
+
+(defn list-enabled-counters
+  [db]
+  (map (fn [[e]]
+         (d/pull db counter-pull e))
+       (d/q '[:find ?e
+              :where
+              [?e :counter/enabled true]]
+            db)))
+
+(defn save-count!
+  [conn id value action]
+  (d/transact conn
+              {:tx-data [{:db/id id
+                          :counter/value value
+                          :counter/action action
+                          :counter/updated-at (now)}]})
+  value)
+
+(defn increment-by!
+  [conn id increment]
+  (let [db (d/db conn)
+        {:counter/keys [value]} (find-counter-by-id db id)
+        next (+ value increment)]
+    (save-count! conn id next "increment")))
 
 (defn disable-counter!
   [conn id]
-  (println "[repo] disable-counter! ->" id)
   (d/transact conn
               {:tx-data [{:db/id id
                           :counter/enabled false
-                          :counter/updated-at (java.util.Date.)
-                          :counter/action "disable"}]}))
+                          :counter/action "disable"
+                          :counter/updated-at (now)}]}))
